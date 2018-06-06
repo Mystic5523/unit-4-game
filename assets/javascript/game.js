368e25802427ae37309334bf5903b1ec5d00671d
Mystic5523 <Mystic5523@hotmail.com>
2018-06-05 22:35:49 -0500

    Done?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,97 @@
-var min = 19;
-        var max = 120;
-        var randomTarget = Math.floor(Math.random() * (max - min + 1)) + min;
-        console.log(randomTarget);
-        document.getElementById("target").textContent = randomTarget;
+var playerWins = 0;
+var playerLosses = 0;
+let topazVal, garnetVal, emeraldVal, diamondVal;
+let input;
 
-       $(document).ready(function() {
-        $("#btnClose1")
-        .text("")
-        .append("<img src=../../assets/images/diamond.png width=100 height=100  />")
-        .button();
-        });
+function updateScore(val) {
+	playerScore = 0;
+	if (val) {
+		input.push(val);
+	}
+	if (input.length >= 1) {
+		for (let i = 0; i < input.length; i++) {
+			playerScore += input[i];
+		}
+	}
+	$('#score').text(playerScore);
+}
 
-        $(document).ready(function() {
-        $("#btnClose2")
-        .text("")
-        .append("<img src=../assets/images/emerald.png width=100 height=100  />")
-        .button();
-        });
+function checkVal() {
+	console.log(playerScore, target);
+	if (playerScore > target) {
+		console.log('You lose.');
+		resetScore();
+		game();
+	} else {
+		if(playerScore < target) {
+			console.log('not yet');
+		} else if (playerScore === target) {
+			console.log('you win!');
+			updateStreak();
+			game();
+		}
+	}
+}
 
-        $(document).ready(function() {
-        $("#btnClose3")
-        .text("")
-        .append("<img src=assets/images/garnet.png width=100 height=100  />")
-        .button();
-        });
+function resetScore() {
+	playerLosses++;
+	playerScore = 0;
+	$('#losses').text(playerLosses);
+}
+function updateStreak() {
+	playerWins++;
+	playerScore = 0;
+	$('#wins').text(playerWins);
+}
 
-        $(document).ready(function() {
-        $("#btnClose4")
-        .text("")
-        .append("<img src=assets/images/topaz.png width=100 height=100  />")
-        .button();
-        });
+function game() {
+	input = [];
+	updateScore();
+	target = randInt(19, 120);
+	$('#target').text(target);
+	topazVal = randInt(1, 12);
+	garnetVal = randInt(1, 12);
+	emeraldVal = randInt(1, 12);
+	diamondVal = randInt(1, 12);
+}
+
+function randInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+game();
+
+$('#topaz').click(function() {
+  updateScore(topazVal);
+  checkVal();
+});
+$('#garnet').click(function() {
+  updateScore(garnetVal);
+  checkVal();
+});
+$('#emerald').click(function(){
+  updateScore(emeraldVal);
+  checkVal();
+});
+$('#diamond').click(function() {
+  updateScore(diamondVal);
+  checkVal();
+});
+
+
+$("#diamond")
+	.text("")
+	.append("<img src='../assets/images/diamond.png' width='80' height='80'  />")
+
+$("#emerald")
+	.text("")
+	.append("<img src='../assets/images/emerald.png' width='80' height='80'  />")
+
+$("#garnet")
+	.text("")
+	.append("<img src='../assets/images/garnet.png' width='80' height='80'  />")
+
+$("#topaz")
+	.text("")
+	.append("<img src='../assets/images/topaz.png' width='80' height='80'  />")
+
+
